4b581556987a66f2a993b3a92c72549d59019ce0
LatticeFramework <10vv3cl@gmail.com>
2023-06-15 19:12:00 -0400

    Add edit button and rough CSS

--- library/main.js
+++ library/main.js
@@ -1,5 +1,7 @@
+// books - main div holding all the books
 const books = document.querySelector('.books');
 
+// array of books
 const myLibrary = [
     {
         title: "Book1",
@@ -15,6 +17,7 @@ const myLibrary = [
     }, 
 ];
 
+// helper function to create html elements with text content and classes
 function createBookElement(el, content, className) {
     const element = document.createElement(el);
     element.textContent = content;
@@ -22,6 +25,7 @@ function createBookElement(el, content, className) {
     return element;
 }
 
+// helper function to create an input checkbox for if book is read w/ event listener
 function createReadElement(bookItem, book) {
     let read = document.createElement("div");
     read.setAttribute("class", "book-read");
@@ -30,23 +34,51 @@ function createReadElement(bookItem, book) {
     input.type = "checkbox";
     input.addEventListener("click", (e) => {
         if(e.target.checked) {
-            bookItem.setAttribute("class", "read-checked")
+            bookItem.setAttribute("class", "card book read-checked")
             book.read = true;
-            renderAndSave();
+            renderBooks();
         } else {
-            bookItem.setAttribute("class", "read-unchecked");
+            bookItem.setAttribute("class", "card book read-unchecked");
             book.read = false;
-            renderAndSave();
+            renderBooks();
         }
     });
     if (book.read) {
         input.checked = true;
-        bookItem.setAttribute("class", "read-checked")
+        bookItem.setAttribute("class", "card book read-checked")
     }
     read.appendChild(input);
     return read;
 }
 
+// create the edit icon w/ event listener
+function createEditIcon(book) {
+    const editIcon = document.createElement("img");
+    editIcon.src = "../icons/pencil.svg";
+    editIcon.setAttribute("class", "edit-icon");
+    editIcon.addEventListener("click", (e) => {
+        console.log(book);
+    });
+    return editIcon;
+}
+
+// create dummy createIcons, they don't do anything
+function createIcons() {
+    const div = createBookElement("div", null, "icons");
+    const icon1 = document.createElement('img')
+    icon1.src = '../icons/star-plus-outline.svg'
+    const icon2 = document.createElement('img')
+    icon2.src = '../icons/eye-plus-outline.svg'
+    const icon3 = document.createElement('img')
+    icon3.src = '../icons/source-branch.svg'
+
+    div.appendChild(icon1);
+    div.appendChild(icon2);
+    div.appendChild(icon3);
+    return div;
+}
+
+// Function to create all the book content on the book dom card
 function createBookItem (book,index) {
     const bookItem = document.createElement('div')
     bookItem.setAttribute('id', index)
@@ -61,15 +93,20 @@ function createBookItem (book,index) {
     bookItem.appendChild(
         createBookElement("h1", `Pages: ${book.pages}`, "book-pages")
     );
-    bookItem.appendChild(createReadElement(bookItem,book))
+    bookItem.appendChild(createReadElement(bookItem,book));
+    bookItem.appendChild(createBookElement("button", "X", "delete"));
+    bookItem.appendChild(createIcons())
+    bookItem.appendChild(createEditIcon(book));
 
     books.insertAdjacentElement("afterbegin", bookItem);
 }
 
+// function to render all the books 
 function renderBooks () {
     myLibrary.map((book, index) => { 
         createBookItem(book,index)
     })
 }
 
+// render on page load
 renderBooks();
